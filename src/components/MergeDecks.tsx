3f53b1e706706204e2e278a317bd0cb6a148c651
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-06-14 10:12:25 -0600

    Update MergeDecks.tsx
    Added Merge Deck Logic

--- src/components/MergeDecks.tsx
+++ src/components/MergeDecks.tsx
@@ -1,6 +1,7 @@
 import Deck from "../classes/Deck"
 import Card from "../classes/Card"
 import { useState, useEffect } from 'react'
+import SuccessAndFailPopUp from "./SuccessAndFailPopUp"
 import axios from "axios"
 
 interface MergeDeckProps {
@@ -12,15 +13,20 @@ interface MergeDeckProps {
 const MergeDeck: React.FC<MergeDeckProps> = ({user, setUser, deck}) => {
     const [secondDeck, setSecondDeck] = useState<Deck | undefined>(undefined)
     const [deckChoices, setDeckChoices] = useState<Deck[]>([])
+    const [success, setSuccess] = useState(false)
+    const [fail, setFail] = useState(false)
+    const [message, setMessage] = useState('')
 
     useEffect(()=>{
-        if (deckChoices.length === 0){
+        if (deckChoices.length == 0){
             let tempDecks = []
             for (let i = 0; i < user.decks.length; i++){
                 if (deck.name !== user.decks[i].name){
                     let cards = []
-                    for (let j = 0; j < user.decks[i]; j++){
-                        cards.push(new Card(user.decks[i].cards[j].question, user.decks[i].cards[j].answer, user.decks[i].cards[j].tries, user.decks[i].cards[j].lastTry))
+                    for (let j = 0; j < user.decks[i].cards.length; j++){
+                        console.log('j=',j)
+                        let newCard = new Card(user.decks[i].cards[j].question, user.decks[i].cards[j].answer, user.decks[i].cards[j].tries, user.decks[i].cards[j].lastTry)
+                        cards.push(newCard)
                     }
                     let newDeck = new Deck (user.decks[i].name, cards)
                     tempDecks.push(newDeck)
@@ -31,19 +37,43 @@ const MergeDeck: React.FC<MergeDeckProps> = ({user, setUser, deck}) => {
     },[])
 
     const handleClick = (index: number) => {
+        console.log(deckChoices[index].deck)
         setSecondDeck(deckChoices[index])
     }
 
     const handleMerge = async () => {
         try{
-            console.log('see todo')
-            //TODO handle Merge logic (update deck with deck.cards.concat(secondDeck.cards)) then delete secondDeck.name
+            let newDeck = deck.deck
+            if (secondDeck){
+                newDeck = newDeck.concat(secondDeck.deck)
+                const _id = user._id
+                let name = deck.name
+                const cards = newDeck
+                console.log(_id, name, cards)
+                let updateResponse = await axios.put('http://localhost:5000/api/updatedeck',{ _id, name, cards })
+                if (updateResponse.data.user){
+                    name = secondDeck.name
+                    let deleteResponse = await axios.delete('http://localhost:5000/api/deletedeck', {data: {_id, name}})
+                    if (deleteResponse.data.user){
+                        setUser(deleteResponse.data.user)
+                        setSuccess(true)
+                        setMessage(`${secondDeck.name} successfully merged into ${deck.name}`)
+                    } else {
+                        throw new Error (`Unable to delete ${name}`)
+                    }
+                } else {
+                    throw new Error (`Unable to update ${name}`)
+                }
+            }
         } catch (error) {
+            setFail(true)
+            setMessage(`${error}`)
             console.error(error)
         }
     }
     return (
         <div className="merge-decks-main">
+            <SuccessAndFailPopUp success={success} setSuccess={setSuccess} fail={fail} setFail={setFail} message={message} name={deck.name} />
             <h2 className="merge-decks-h2">Select a second deck to merge into {deck.name}</h2>
             {deckChoices.map((deck1, index)=>(<div>
                 <button onClick={()=>handleClick(index)} className="merge-decks-button">{deck1.name}</button>
