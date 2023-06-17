8285eb1eea7b5ad01bc56cd5c2be76e0b4028d25
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-06-16 21:17:26 -0600

    Add Cards With AI

--- src/components/AddCardsFromAI.tsx
+++ src/components/AddCardsFromAI.tsx
@@ -1,30 +1,141 @@
-import Deck from "../classes/Deck"
+import Deck from "../classes/Deck";
+import Card from "../classes/Card";
+import { useState } from 'react';
+import SimpleDeckView from "./SimpleDeckView";
+import SuccessAndFailPopUp from "./SuccessAndFailPopUp";
+import axios from 'axios'
+
+import '../styles/AddCardsFromAI.css'
 
 interface AddCardsFromAIProps {
-    user: any,
-    setUser: React.Dispatch<React.SetStateAction<any>>
-    deck: Deck
+  user: any;
+  setUser: React.Dispatch<React.SetStateAction<any>>;
+  deck: Deck;
 }
 
-let numbers = [5, 10, 15, 20, 25, 30]
+let numbers = [5, 10, 15, 20];
 
-const AddCardsFromAI: React.FC<AddCardsFromAIProps> = ({user, setUser, deck}) => {
-    return (
-        <div className="add-cards-from-ai-main">
-            <h2>Add cards to {deck.name}</h2>
-            <form>
-                <label>Topic:</label>
-                <input type="text" id="topic"></input>
-                <label>Sub Topic:</label>
-                <input type="text" id="sub-topic"></input>
-                <label>Select a number between 5 and 20</label>
-                <select id="numberSelect" name="numberSelect">
-                    {numbers.map(x => <option value={x}>{x}</option>)}
-                </select>
-                <button type="submit">Make Cards</button>
-            </form>
-        </div>
-    )
+const AddCardsFromAI: React.FC<AddCardsFromAIProps> = ({ user, setUser, deck }) => {
+  const [aiTokens, setAiTokens] = useState(user.ai_tokens);
+  const [topic, setTopic] = useState('');
+  const [subTopic, setSubTopic] = useState('');
+  const [num, setNum] = useState(0);
+  const [confirmPopup, setConfirmPopup] = useState(false);
+  const [cards, setCards] = useState<string[][]>([])
+  const [success, setSuccess] = useState(false)
+  const [fail, setFail] = useState(false)
+  const [message, setMessage] = useState('')
+
+  const handleTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTopic(event.target.value);
+  };
+
+  const handleSubTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSubTopic(event.target.value);
+  };
+
+  const handleNumChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setNum(Number(event.target.value));
+  };
+
+  const handleSave = async () => {
+    const cardArr = []
+    for (let i = 0; i < cards.length ; i++){
+        cardArr.push(new Card(cards[i][0], cards[i][1]))
+    }
+    const saveCards = cardArr.map(item=>{
+        return {question: item.question, answer: item.answer, tries: item.tries, lastTry: item.lastTry}
+    })
+    const _id = user._id
+    const name = deck.name
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_URL}api/add`, {"_id": _id, "name": name, "cards": saveCards})
+        if (response.data.user){
+            setUser(response.data.user)
+            setSuccess(true)
+            setMessage(`Saved ${deck.name} with ${saveCards.length} cards`)
+        } else {
+            setFail(true)
+            setMessage(`Unable to save ${deck.name} to database, try again later`)
+        }
+    } catch (error){
+        setFail(true)
+        setMessage(`Unable to save ${deck.name} to database, try again later`)
+        console.error(error)
+    }
 }
 
-export default AddCardsFromAI
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const submitHelper = async () => {
+      try{
+        const _id = user._id
+        const number = num
+        const response = await axios.post(`${process.env.REACT_APP_URL}api/cardsfromai`, {_id, number, topic, subTopic})
+        if (response.data.user){
+          console.log(response.data.cards)
+          if (response.data.cards){
+            let tempCards = []
+            for (let i = 0; i < response.data.cards.length; i++){
+              tempCards.push([response.data.cards[i].question, response.data.cards[i].answer])
+            }
+            setCards(cards.concat(tempCards))
+          }
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    submitHelper()
+  };
+
+  return (
+    <div className="add-cards-from-ai-main">
+      <SuccessAndFailPopUp success={success} setSuccess={setSuccess} fail={fail} setFail={setFail} message={message} name={deck.name}/>
+      <h2 className="add-cards-from-ai-h2">Add cards to {deck.name}</h2>
+      <form onSubmit={handleSubmit} className="add-cards-from-ai-form">
+        <label className="add-cards-from-ai-label">Topic:</label>
+        <input
+          type="text"
+          id="topic"
+          className="add-cards-from-ai-input"
+          value={topic}
+          onChange={handleTopicChange}
+          required
+        />
+        <label className="add-cards-from-ai-label">Sub Topic:</label>
+        <input
+          type="text"
+          id="sub-topic"
+          className="add-cards-from-ai-input"
+          value={subTopic}
+          onChange={handleSubTopicChange}
+          required
+        />
+        <label className="add-cards-from-ai-label">Select a number between 5 and 20</label>
+        <select
+          id="numberSelect"
+          name="numberSelect"
+          className="number-select"
+          value={num}
+          onChange={handleNumChange}
+          required
+        >
+          {numbers.map((x) => (
+            <option value={x} key={x}
+              className="add-cards-from-ai-option"
+            >
+              {x}
+            </option>
+          ))}
+        </select>
+        <button type="submit" className="add-cards-from-ai-button">Make Cards</button>
+      </form>
+      <button onClick={()=>console.log(cards)}>CARDS</button>
+      <button onClick={handleSave}>Save</button>
+      <SimpleDeckView deck={cards} setDeck={setCards} />
+    </div>
+  );
+};
+
+export default AddCardsFromAI;
