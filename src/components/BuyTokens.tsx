ee0acc26c22968e4a66f14834bcc2f9b4eb655cd
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-06-25 14:20:18 -0600

    BuyToken and Dashboard Style

--- src/components/BuyTokens.tsx
+++ src/components/BuyTokens.tsx
@@ -1,5 +1,8 @@
 import Checkout from "./Checkout"
 import TopBar from "./TopBar"
+
+import { useNavigate } from "react-router-dom"
+
 import '../styles/BuyTokens.css'
 
 interface BuyTokensProps {
@@ -8,13 +11,17 @@ interface BuyTokensProps {
 }
 
 const BuyTokens: React.FC<BuyTokensProps> = ({user, setUser}) => {
+    const navigate = useNavigate()
     return (
         <div className="buy-tokens-background">
             <TopBar user={user} setUser={setUser}/>
             <div className="buy-tokens-main">
                 <h2 className="buy-tokens-h2">Buy 100k AI Tokens for $5.00 CAD</h2>
                 <Checkout user={user} setUser={setUser} />
-                <p className="buy-tokens-p">Payments are processed by our parter STRIPE. Click <a href="https://stripe.com/en-ca/payments" target="_blank">here</a> to learn more.</p>
+                <div className="buy-tokens-bottom-container">
+                    <p className="buy-tokens-p">Payments are processed by our parter STRIPE. Click <a href="https://stripe.com/en-ca/payments" target="_blank">here</a> to learn more.</p>
+                    <button className="buy-tokens-back-button" onClick={()=>navigate('/store')}>Back</button>
+                </div>
             </div>
         </div>
     )
